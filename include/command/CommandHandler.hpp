340855a0c92c0ff93fe60fcdba51a3c9f74c2f7d
--help <management.tadrosnicolas@gmail.com>
2026-07-07 15:19:10 +0200

    impl: CommandHandler stubs

---
+++ include/command/CommandHandler.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <iostream>
+#include <map>
+
+#include "core/Server.hpp"
+
+struct Command {
+    std::string prefix;
+    std::string command;
+    std::string params;
+};
+
+class CommandHandler {
+    private:
+        Server& _server;
+        typedef void (CommandHandler::*Handler)(Client& client, const Command& cmd);
+        std::map<std::string, Handler>  _cmds;
+
+        void    _pass(Client& client, const Command& cmd);
+        void    _nick(Client& client, const Command& cmd);
+        void    _join(Client& client, const Command& cmd);
+        void    _user(Client& client, const Command& cmd);
+        void    _quit(Client& client, const Command& cmd);
+
+    public:
+        CommandHandler(Server& server);
+        ~CommandHandler();
+};
