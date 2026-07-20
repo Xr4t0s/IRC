8b1ef1a5c2b2101bd9c184e3b7bd22552bc856f7
Enzo Giacomoni <enzo.giacomoni66@gmail.com>
2026-07-20 20:28:12 +0200

    impl: stubs channels

--- src/core/Channel.cpp
+++ src/core/Channel.cpp
@@ -1,4 +1,10 @@
 #include <core/Channel.hpp>
 
 Channel::Channel() {}
+
+// Join command :
+Channel::Channel(Client * client, std::string name) : _name(name) {
+    _clients.push_back(client);
+}
+
 Channel::~Channel() {}
