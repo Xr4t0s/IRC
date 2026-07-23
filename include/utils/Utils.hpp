b3bdba88de28cdedb709c30c0d5c2d9dbd4c9082
Xr4t0s <nicolastadros.maillol@gmail.com>
2026-07-23 19:05:58 +0200

    utils: added splitBy() function

--- include/utils/Utils.hpp
+++ include/utils/Utils.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 
 std::string intToString(int nb);
+std::vector<std::string> splitBy(const std::string& full, const char sep);
--- src/utils/Utils.cpp
+++ src/utils/Utils.cpp
@@ -6,3 +6,25 @@ std::string intToString(int nb) {
     oss << nb;
     return oss.str();
 }
+
+std::vector<std::string> splitBy(const std::string& full, const char sep)
+{
+    std::vector<std::string> result;
+    std::string current;
+
+    for (std::string::size_type i = 0; i < full.size(); ++i)
+    {
+        if (full[i] == sep)
+        {
+            result.push_back(current);
+            current.clear();
+        }
+        else
+        {
+            current += full[i];
+        }
+    }
+
+    result.push_back(current);
+    return result;
+}
